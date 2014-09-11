85465ee353e7bc964119a9c6390e823f12438ab6
Patrick Gombert <patrickgombert@gmail.com>
2014-09-10 22:54:16 -0500

    Evaluate simple types and namespace resolution.

---
+++ Runtime.hs
@@ -0,0 +1,59 @@
+module Runtime where
+
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.State
+import qualified Data.Map as M
+
+type LispM a = StateT Environment IO a
+
+type Bindings = M.Map Value Value
+data Environment = Environment { parent :: Maybe Environment, binding :: Bindings } deriving (Ord, Show, Eq)
+
+data Value = Vint Int
+           | Vfloat Float
+           | Vlist [Value]
+           | Vbool Bool
+           | Vstr String
+           | Vsym (Maybe Value) Value
+           | Vmap Bindings
+           | Vlambda [Value] Environment [Expression] deriving (Ord, Show, Eq)
+
+data Expression = Eint Int
+                | Efloat Float
+                | Ebool Bool
+                | Estr String
+                | Esym (Maybe Value) Value
+                | EMap Bindings
+                | Elist [Expression] deriving (Ord, Show, Eq)
+
+lispEval :: Expression -> LispM Value
+lispEval (Eint e) = return (Vint e)
+lispEval (Efloat e) = return (Vfloat e)
+lispEval (Ebool e) = return (Vbool e)
+lispEval (Estr e) = return (Vstr e)
+lispEval (Esym ns str) = lookupSym ns str
+
+lookupSym :: Maybe Value -> Value -> LispM Value
+lookupSym ns symbol@(Vstr symName) = do
+  env <- get
+  case ns of
+    Just ns'@(Vstr nsName) -> case M.lookup ns' (binding env) of
+                           Just (Vmap nsBindings) -> case M.lookup symbol nsBindings of
+                                                       Just result -> return result
+                                                       Nothing -> error $ "Symbol " ++ symName ++ " not found in namespace " ++ nsName
+                           Nothing -> error $ "Referenced namespace " ++ nsName ++ " but it did not exist"
+    Just ns' -> error $ "Value " ++ (show ns') ++ " given as namespace for symbol"
+    Nothing -> case M.lookup symbol (binding env) of
+                 Just result -> return result
+                 Nothing -> return (Vsym Nothing symbol)
+lookupSym _ symbol = error $ "Value " ++ (show symbol) ++ " given as a symbol (expecting a string)"
+
+globalEnvironment :: Environment
+globalEnvironment = (Environment Nothing M.empty)
+
+run :: [Expression] -> IO Value
+run expressions = do
+  let action = sequence (map lispEval expressions)
+  (result, _) <- runStateT action globalEnvironment
+  return (last result)
