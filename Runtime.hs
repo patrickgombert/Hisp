ffd317cbf6213b0d5a9bce2b326005d5e6d2ddd4
Patrick Gombert <patrickgombert@gmail.com>
2014-11-04 17:26:10 +0000

    Implement not equals

--- Runtime.hs
+++ Runtime.hs
@@ -176,20 +176,23 @@ run expressions = do
 globalEnvironment :: Environment
 globalEnvironment = Environment Nothing Nothing $ M.fromList [
                                                               ((Vsym Nothing (Vstr "print-line")), printLine),
-                                                              ((Vsym Nothing (Vstr "=")), equals)
+                                                              ((Vsym Nothing (Vstr "=")), equals),
+                                                              ((Vsym Nothing (Vstr "!=")), notEquals)
                                                              ]
 
 printLine :: Value
 printLine = Vbuiltinfn "print-line" (\vals -> do lift $ mapM_ (putStrLn . show) vals
                                                  return (Vsym Nothing (Vstr "null")))
-
-equalsReduce :: Bool -> [Value] -> Value
+equalsReduce :: Bool -> [Value] -> Bool
 equalsReduce state (v:vs) = if state
                               then if length vs == 0
-                                   then (Vbool state)
+                                   then state
                                    else equalsReduce (and [state, (v == (head vs))]) vs
-                              else (Vbool False)
+                              else False
 
 equals :: Value
-equals = Vbuiltinfn "=" (\vals -> return (equalsReduce True vals))
+equals = Vbuiltinfn "=" (\vals -> return (Vbool (equalsReduce True vals)))
+
+notEquals :: Value
+notEquals = Vbuiltinfn "!=" (\vals -> return (Vbool (not (equalsReduce True vals))))
 
